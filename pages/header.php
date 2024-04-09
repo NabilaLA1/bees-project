1e623816a10230ee8f7070d59dca931e926f256f
Your Name <muhdsanee87@gmail.com>
2024-04-09 11:01:56 +0100

    added footer and header

---
+++ pages/header.php
@@ -0,0 +1,18 @@
+<header>
+        <!--logo-->
+        <img class="logo" src="./icons/logo.svg" alt="logo">
+        <nav>
+        <!--menu-->
+            <ul class="nav-links" id="menuList">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#category">Category</a></li>
+                <li><a href="#Contact-us">Contact Us</a></li>
+            </ul>
+        </nav>
+        <div class="open">
+            <img src="./icons/bars-solid.svg" class="menu-icon" onclick="togglemenu()">
+        </div>
+        <!--log in button--> 
+        <a class="btn" href="login.php"><button>Log in</button></a>
+    </header>
